1cb5295a7a6f57ef1a03c0c2a33fa6f142428ab6
Lag <f.gete@ludus-academie.com>
2021-03-21 23:20:25 +0100

    Engine 1.1.0 -- shader basic functions

--- LAG_Engine_OpenGL/src/Application.cpp
+++ LAG_Engine_OpenGL/src/Application.cpp
@@ -1,7 +1,49 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#include <malloc.h>
 #include <iostream>
+
+// TUTO-7 21-03-2021 Shader compiler
+static unsigned int CompileShader(unsigned int type, const std::string& source) {
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE) {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)_malloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile " << (type == GL_VERTEX_SHADER ? "vertex" : "fragment") << " shader !" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+
+        return 0;
+    }
+
+    return id;
+}
+
+// TUTO-7 21-03-2021 Shader creater
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader) {
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
 
 int main(void)
 {
@@ -22,11 +64,49 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    // TUTO-2 20-03-2021
+    // TUTO-3 20-03-2021 (GLEW setup)
     if (glewInit() != GLEW_OK)
         std::cout << "ERROR -- glew could not initialize !" << std::endl;
 
     std::cout << glGetString(GL_VERSION) << std::endl;
+
+    // TUTO-4 21-03-2021 (vertex buffer)
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+    unsigned int buffer; // will be the buffer's id
+
+    glGenBuffers(1, &buffer); // Generate 1 buffer with (int)buffer id
+    glBindBuffer(GL_ARRAY_BUFFER, buffer); // Specify buffer data location
+    glBufferData(GL_ARRAY_BUFFER, 6*sizeof(float), positions, GL_STATIC_DRAW); // Set buffer data and use
+
+    // TUTO-5 21-03-2021 vertex attribute
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), 0);
+
+    // TUTO-7 21-03-2021
+    std::string vertexShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) in vec4 position;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n";
+    std::string fragmentShader =
+        "#version 330 core\n"
+        "\n"
+        "layout(location = 0) out vec4 color;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n";
+    unsigned int shader = CreateShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
@@ -34,12 +114,17 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // TUTO-1 20-03-2021 (test triangle)
+        // TUTO-2 20-03-2021 (test triangle)
+        /*
         glBegin(GL_TRIANGLES);
         glVertex2f(-.5f, -.5f);
-        glVertex2f(.0f, .5f);
-        glVertex2f(.5f, -.5f);
+        glVertex2f( .0f,  .5f);
+        glVertex2f( .5f, -.5f);
         glEnd();
+        */
+
+        // TUTO-4 21-03-2021
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -47,6 +132,9 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    // TUTO-7 21-03-2021
+    glDeleteProgram(shader);
 
     glfwTerminate();
     return 0;
